adf241dc44efda05a46ab9dfdd5c91d6882f9b35
DenMitter <57758gfk@gmail.com>
2024-09-01 12:18:54 +0300

    add api users ( index )

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -26,6 +26,11 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'company' => fake()->company(),
+            'position' => fake()->word(),
+            'avatar' => '/assets/avatars/avatar-' . rand(1, 7) . '.jpg',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
